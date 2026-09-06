12f5362f336e9dd8c562c93359f25822c0948571
rishitamisra <rishitagungun12@gmail.com>
2026-09-06 13:06:33 +0530

    Remove: is-subsequence - submission-0

--- Data Structures & Algorithms/is-subsequence/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        for i in s:
-            if i in t:
-                pass
-            else:
-                return False
-        return True
